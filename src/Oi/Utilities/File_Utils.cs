4c0630028dfb3d99cc918df52c2e191210114d4d
Gavin Crump <58870499+aussieBIMguru@users.noreply.github.com>
2026-08-01 15:43:42 +0930

    Updates
    New link methods

--- src/Oi/Utilities/File_Utils.cs
+++ src/Oi/Utilities/File_Utils.cs
@@ -1,4 +1,5 @@
 ﻿// The class belongs to the Utilities namespace
+using System.Diagnostics;
 using System.IO;
 
 namespace Oi.Utilities
@@ -8,6 +9,45 @@ namespace Oi.Utilities
     /// </summary>
     public static class File_Utils
     {
+        /// <summary>
+        /// Used to verify if a URL is valid (will open).
+        /// </summary>
+        /// <param name="linkPath">The path, typically a URL.</param>
+        /// <returns>A Boolean.</returns>
+        public static bool LinkIsAccessible(string linkPath)
+        {
+            return Uri.TryCreate(linkPath, UriKind.Absolute, out Uri uriResult)
+                   && (uriResult.Scheme == Uri.UriSchemeHttp
+                   || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+
+        /// <summary>
+        /// Attempts to open a link in the default browser.
+        /// </summary>
+        /// <param name="linkPath">The path, typically a URL.</param>
+        /// <returns>A Result.</returns>
+        public static Result OpenLinkPath(string linkPath)
+        {
+            if (LinkIsAccessible(linkPath))
+            {
+                try
+                {
+                    Process.Start(new ProcessStartInfo { FileName = linkPath, UseShellExecute = true });
+                    return Result.Succeeded;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred while trying to open the URL: {ex.Message} ({linkPath})");
+                    return Result.Cancelled;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"ERROR: Link path could not be opened ({linkPath})");
+                return Result.Cancelled;
+            }
+        }
+
         /// <summary>
         /// Runs an accessibility check on a file path.
         /// </summary>
